0b79333af232fce189c965df7dcf06a4a29924d0
RealOST <zengzhiyuan1113@outlook.com>
2024-09-05 11:15:50 +0800

    update

---
+++ Assets/Scripts/Core/CoreComponents/Death.cs
@@ -0,0 +1,29 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Death : CoreComponent {
+    [SerializeField] private GameObject[] deathParticles;
+
+    private ParticleManager ParticleManager => particleManager ??= core.GetCoreComponent<ParticleManager>();
+    private ParticleManager particleManager;
+
+    private Stats Stats => stats ??= core.GetCoreComponent<Stats>();
+    private Stats stats;
+    
+    public void Die() {
+        foreach (var particle in deathParticles) {
+            ParticleManager.StartParticles(particle);
+        }
+        
+        core.transform.parent.gameObject.SetActive(false);
+    }
+
+    private void OnEnable() {
+        Stats.OnHealthZero += Die;
+    }
+
+    private void OnDisable() {
+        Stats.OnHealthZero -= Die;
+    }
+}
